902d4942d40197d816a153f2b323e85e3be65957
vargasol <66957125+vargasol@users.noreply.github.com>
2026-04-07 14:53:44 +0200

    Create devops-agent-install.sh

---
+++ devops-agent-install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Install Azure Devops Agent as service"
+
+ORG_URL=$1
+PAT="$2"
+POOL="$3"
+AGENT="$4"
+
+sudo apt update
+sudo apt install -y curl wget apt-transport-https
+
+mkdir -p ~/myagent && cd ~/myagent
+wget https://download.agent.dev.azure.com/agent/4.271.0/vsts-agent-linux-x64-4.271.0.tar.gz
+tar zxvf vsts-agent-linux-x64-4.271.0.tar.gz
+./config.sh --unattended \
+  --url $ORG_URL \
+  --auth pat \
+  --token $PAT \
+  --pool $POOL \
+  --agent $AGENT \
+  --acceptTeeEula
+sudo ./svc.sh install
+sudo ./svc.sh start
+
+rm vsts-agent-linux-x64-4.271.0.tar.gz
+
+echo "Install Azure CLI"
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
