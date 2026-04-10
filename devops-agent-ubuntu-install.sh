6391d2e61b0f7871a25bcd2d1fc4781cb316ef4b
vargasol <66957125+vargasol@users.noreply.github.com>
2026-04-10 16:07:57 +0200

    Update devops-agent-ubuntu-install.sh

--- devops-agent-ubuntu-install.sh
+++ devops-agent-ubuntu-install.sh
@@ -11,20 +11,17 @@ export BASE_DIR=/opt/vsts-agent-linux
 sudo apt update
 sudo apt install -y curl wget apt-transport-https
 
-mkdir -p ~/myagent && cd ~/myagent
-mkdir $BASE_DIR
-cd $BASE_DIR
 wget https://download.agent.dev.azure.com/agent/4.271.0/vsts-agent-linux-x64-4.271.0.tar.gz -P $BASE_DIR
 tar -zxvf $BASE_DIR/vsts-agent-linux-x64-4.271.0.tar.gz
-./config.sh --unattended \
+$BASE_DIR./config.sh --unattended \
   --url $ORG_URL \
   --auth pat \
   --token $PAT \
   --pool $POOL \
   --agent $AGENT \
   --acceptTeeEula
-sudo ./svc.sh install
-sudo ./svc.sh start
+sudo $BASE_DIR./svc.sh install
+sudo $BASE_DIR./svc.sh start
 
 rm vsts-agent-linux-x64-4.271.0.tar.gz
 
